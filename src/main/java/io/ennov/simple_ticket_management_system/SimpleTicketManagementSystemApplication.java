b4e32ee6931dd9bb4b2de104c21be42a76681293
Serge KOUAME <sergekouame@192.168.1.9>
2024-09-15 23:51:56 +0000

    project initialization

---
+++ src/main/java/io/ennov/simple_ticket_management_system/SimpleTicketManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package io.ennov.simple_ticket_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTicketManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTicketManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ennov/simple_ticket_management_system/SimpleTicketManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ennov.simple_ticket_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTicketManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
